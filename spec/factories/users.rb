fa475fffd3c41b85c1a7d217af81591dbaf70656
Kotaro126 <68246581+Kotaro126@users.noreply.github.com>
2020-10-17 22:55:51 +0900

    Revert "Revert "ユーザー管理機能の実装""
    This reverts commit c178011dadf1ae97d06f8b9c96428ae38e9a1555.

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.initials}
+    email                 {Faker::Internet.free_email}
+    password              {"kkk111"}
+    password_confirmation {password}
+    first_name            {"山田"}
+    last_name             {"健吾"}
+    first_name_kana       {"ヤマダ"}
+    last_name_kana        {"ケンゴ"}
+    birthday              {"1995-09-15"}
+  end
+end
