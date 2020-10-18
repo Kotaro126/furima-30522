b44dc795bf4caa0c8785645ecc6f57a50ed64ead
Kotaro126 <68246581+Kotaro126@users.noreply.github.com>
2020-10-18 19:00:04 +0900

    レビュー修正済み1回目

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-  validates :nickname, presence: :true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+  end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
@@ -8,12 +11,11 @@ class User < ApplicationRecord
     validates :first_name
     validates :last_name
   end
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角文字を使用してください'} do
+
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナを使用してください'} do
     validates :first_name_kana
     validates :last_name_kana
   end
-
-  validates :birthday, presence: :true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
